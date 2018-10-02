d34c11bfce8170e2a26ea50be9df71dffc94e104
pra2win <pra2win@gmail.com>
2018-10-02 22:35:43 +0530

    Adding Product type enum

--- Istriwala.API/Istriwala.Core/Constants.cs
+++ Istriwala.API/Istriwala.Core/Constants.cs
@@ -8,11 +8,23 @@ namespace Istriwala.Core
 {
     public class Constants
     {
+        /// <summary>
+        /// Must be sync with dbo.Role table
+        /// </summary>
         public enum Roles
         {
             Client = 1,
             Admin = 2,
             SuperAdmin = 3
+        }
+
+        /// <summary>
+        /// must be sync with ProductType table
+        /// </summary>
+        public enum ProductTypes
+        {
+            Male = 1,
+            Female = 2
         }
     }
 }
